48b3995125597d4031ab2d79c2d1c7c0a43d5761
Anmeng12 <1283291837@qq.com>
2025-10-22 14:58:21 +0800

    Add files via upload

--- shangji 2/shangji 4.2  .c
+++ shangji 2/shangji 4.2  .c
@@ -4,14 +4,13 @@
 #define PI 3.1415926
 
 int main() {
-    // 输入圆半径（值为5）
+    
     double radius = 5.0;
 
-    // 计算圆的周长和面积
     double circumference = 2 * PI * radius;
     double area = PI * radius * radius;
 
-    // 输出结果，精确到小数点后3位
+    
     printf("半径为%.1f的圆：\n", radius);
     printf("其周长 = %.3f\n", circumference);
     printf("其面积 = %.2f\n", area);
